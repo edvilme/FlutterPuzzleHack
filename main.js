849e6af3c97d1fc24ed2cb0374448a43611c23c6
Eduardo Villalpando Mello <eduardo.villalpando.mello@gmail.com>
2022-01-09 19:17:47 -0600

    Added animations

--- main.js
+++ main.js
@@ -1,86 +1,90 @@
+class BoardTile {
+    static count = 0;
+    constructor(value, i, j){
+        this.value = value;
+        this.i = i;
+        this.j = j;
+        this.index = BoardTile.count++;
+    }
+}
+
 class Board{
     constructor(n){
-        this.pieces = [
+        const data = [
             ['A', 'B', 'C', 'D'], 
             ['E', 'F', 'G', 'H'], 
-            ['I', 'J', 'K', '*'], 
+            ['I', 'J', 'K', ''], 
             ['M', 'N', 'O', 'L']
         ]
-        this.emptyPosition = [2, 3];
-    }
-    getFreeColumn(){
-        let result = [];
-        for(let row of this.pieces){
-            result.push(row[this.emptyPosition[1]]);
-        }
-        return result;
-    }
-    getFreeRow(){
-        return this.pieces[this.emptyPosition[0]];
+        this.pieces = data.map((row, i) => 
+            row.map((cell, j) => new BoardTile(cell, i, j))    
+        )
+        this.emptyTile = this.pieces[2][3];
     }
     print(){
         for(let row of this.pieces){
-            console.log(row.join('\t'))
+            console.log(row.map(cell => cell.value).join('\t'))
         }
     }
     swapInDirection(direction){
         // console.log(`Swap ${direction}`)
-        if(direction == 'up' && this.emptyPosition[0] > 0){
-            let curr = this.pieces[this.emptyPosition[0] - 1][this.emptyPosition[1]];
-            this.pieces[this.emptyPosition[0]][this.emptyPosition[1]] = curr;
-            this.emptyPosition[0]--;
+        if(direction == 'up' && this.emptyTile.i > 0){
+            let curr = this.pieces[this.emptyTile.i - 1][this.emptyTile.j];
+            this.pieces[this.emptyTile.i][this.emptyTile.j] = curr;
+            curr.i = this.emptyTile.i;
+            curr.j = this.emptyTile.j;
+            this.emptyTile.i--;
         }
-        if(direction == 'down' && this.emptyPosition[0] < this.pieces.length - 1){
-            let curr = this.pieces[this.emptyPosition[0] + 1][this.emptyPosition[1]];
-            this.pieces[this.emptyPosition[0]][this.emptyPosition[1]] = curr;
-            this.emptyPosition[0]++;
+        if(direction == 'down' && this.emptyTile.i < this.pieces.length - 1){
+            let curr = this.pieces[this.emptyTile.i + 1][this.emptyTile.j];
+            this.pieces[this.emptyTile.i][this.emptyTile.j] = curr;
+            curr.i = this.emptyTile.i;
+            curr.j = this.emptyTile.j;
+            this.emptyTile.i++;
         }
-        if(direction == 'left' && this.emptyPosition[1] > 0){
-            let curr = this.pieces[this.emptyPosition[0]][this.emptyPosition[1] - 1];
-            this.pieces[this.emptyPosition[0]][this.emptyPosition[1]] = curr;
-            this.emptyPosition[1]--;
+        if(direction == 'left' && this.emptyTile.j > 0){
+            let curr = this.pieces[this.emptyTile.i][this.emptyTile.j - 1];
+            this.pieces[this.emptyTile.i][this.emptyTile.j] = curr;
+            curr.i = this.emptyTile.i;
+            curr.j = this.emptyTile.j;
+            this.emptyTile.j--;
         }
-        if(direction == 'right' && this.emptyPosition[1] < this.pieces.length - 1){
-            let curr = this.pieces[this.emptyPosition[0]][this.emptyPosition[1] + 1];
-            this.pieces[this.emptyPosition[0]][this.emptyPosition[1]] = curr;
-            this.emptyPosition[1]++;
+        if(direction == 'right' && this.emptyTile.j < this.pieces.length - 1){
+            let curr = this.pieces[this.emptyTile.i][this.emptyTile.j + 1];
+            this.pieces[this.emptyTile.i][this.emptyTile.j] = curr;
+            curr.i = this.emptyTile.i;
+            curr.j = this.emptyTile.j;
+            this.emptyTile.j++;
         }
-        this.pieces[this.emptyPosition[0]][this.emptyPosition[1]] = '*';
+        this.pieces[this.emptyTile.i][this.emptyTile.j] = this.emptyTile;
     }
     swapToPosition(i, j){
-        if(this.emptyPosition[0] == i && this.emptyPosition[1] == j) 
+        if(this.emptyTile.i == i && this.emptyTile.j == j) 
             return; 
-        if(this.emptyPosition[0] == i){
+        if(this.emptyTile.i == i){
             // Swap left
-            while(this.emptyPosition[1] > j)
+            while(this.emptyTile.j > j)
                 this.swapInDirection('left');
             // Swap right
-            while(this.emptyPosition[1] < j)
+            while(this.emptyTile.j < j)
                 this.swapInDirection('right');
         }
-        if(this.emptyPosition[1] == j){
+        if(this.emptyTile.j == j){
             // Swap up
-            while(this.emptyPosition[0] > i)
+            while(this.emptyTile.i > i)
                 this.swapInDirection('up');
             // Swap down
-            while(this.emptyPosition[0] < i)
+            while(this.emptyTile.i < i)
                 this.swapInDirection('down');
         }
+        return this.getPieces();
     }
-
     shuffle(n = 20){
         for(let i = 0; i < n; i++){
             this.swapInDirection(['left', 'up', 'right', 'down'][ Math.floor(Math.random()*4) ]);
         }
     }
+    getPieces(){
+        return this.pieces.flat().sort((a, b) => a.index - b.index);
+    }
 }
-
-const b = new Board();
-// b.shuffle();
-b.print();
- console.log('----')
- b.swapToPosition(0, 3)
- b.print()
- console.log('----')
-b.swapToPosition(0, 3)
-b.print()
